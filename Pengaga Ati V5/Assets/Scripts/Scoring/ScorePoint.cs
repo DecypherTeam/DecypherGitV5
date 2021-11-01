53a60fadb35dd5aed7cef106fca13d35aa87ba00
decypherGit <decyphergit@gmail.com>
2021-11-01 15:23:41 +0800

    Added score system

--- Pengaga Ati V5/Assets/Scripts/Scoring/ScorePoint.cs
+++ Pengaga Ati V5/Assets/Scripts/Scoring/ScorePoint.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class ScorePoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Plant")
         {
             Destroy(other.gameObject);
             Debug.Log("Chillie is successfully delivered");
+            ScoringSystem.theScore += 1;
         }
     }
 }
