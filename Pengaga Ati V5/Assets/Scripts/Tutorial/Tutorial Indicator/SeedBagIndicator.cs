139839386ded2d88f53e549283c7cc76b8044a8a
decypherGit <decyphergit@gmail.com>
2021-11-22 22:30:31 +0800

    Add script to seedbag to spawn indicator

--- Pengaga Ati V5/Assets/Scripts/Tutorial/Tutorial Indicator/SeedBagIndicator.cs
+++ Pengaga Ati V5/Assets/Scripts/Tutorial/Tutorial Indicator/SeedBagIndicator.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeedBagIndicator : MonoBehaviour
+namespace Examples
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SeedBagIndicator : MonoBehaviour
     {
-        
-    }
+        public GameObject indicator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            SpawnUI();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void SpawnUI()
+        {
+            Vector3 pos = new Vector3(0f, 0.1f, 0f);
+            Instantiate(indicator, transform.position + pos, transform.rotation);
+        }
     }
 }
